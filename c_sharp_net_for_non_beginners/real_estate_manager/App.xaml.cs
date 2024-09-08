6799abdd9c77d5fbff92b5f8151d81825e2beb1a
warmlund <emeliewarmlund@gmail.com>
2024-09-08 12:30:56 +0200

    Startup of assignemnt 1, a real estate manager

---
+++ c_sharp_net_for_non_beginners/real_estate_manager/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace real_estate_manager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
